cc5033a2212b0c45ebb1717c7b202d8c2beec13f
jules.corneille <jules.corneille@uha.fr>
2023-09-28 10:25:08 +0200

    Tout les constructeur de la meme origine sont enssemble

--- constructeur.php
+++ constructeur.php
@@ -16,23 +16,30 @@
         </div>
     </a>
     <div class="scroller">        
-        <?php
-            $api_url = ('https://filrouge.uha4point0.fr/V2/car/constructeurs');
+    <?php
+            $api_url = 'https://filrouge.uha4point0.fr/V2/car/constructeurs';
             $ch = curl_init($api_url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $json_data = curl_exec($ch);
             $response_data = json_decode($json_data);
-            if (is_array($response_data)){
-            foreach ($response_data as $item) {
-                    echo '<div class="partie"><div class="pays"><img src="./img/drapeau_svg/'. $item->pays .'.svg" class="imgpays"><h1 class="txtpays">'. $item->pays .'</h1></div><div class="marques"><a href="./template_marque.php?id='. $item->id .'"><div class="boutondiv"><img src="./img/logo_marque/'. $item->nom .'.svg" class="logoimg"></div></a></div><hr></div>';
+            if (is_array($response_data)) {
+                $marquesParPays = [];
+                foreach ($response_data as $item) {
+                    $pays = $item->pays;
+                    $marque = '<a href="./template_marque.php?id=' . $item->id . '"><div class="boutondiv"><img src="./img/logo_marque/' . $item->nom . '.svg" class="logoimg"></div></a>';
+                    if (!isset($marquesParPays[$pays])) {
+                        $marquesParPays[$pays] = $marque;
+                    } else {
+                        $marquesParPays[$pays] .= $marque;
+                    }
+                }
+                foreach ($marquesParPays as $pays => $marques) {
+                    echo '<div class="partie"><div class="pays"><img src="./img/drapeau_svg/' . $pays . '.svg" class="imgpays"><h1 class="txtpays">' . $pays . '</h1></div><div class="marques">' . $marques . '</div><hr></div>';
                 }
             }
-            
+
             curl_close($ch);
         ?>
-
-
-        
     </div>
 </body>
 </html>
