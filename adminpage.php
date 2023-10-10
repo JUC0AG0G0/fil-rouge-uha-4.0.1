a844af146780dc3deb499e04c60d145c871e3698
jules.corneille <jules.corneille@uha.fr>
2023-10-10 11:45:27 +0200

    creation d'une page pour admininstratrer le site (aucun bouton fonctionne )

---
+++ adminpage.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>admin page</title>
+    <link rel="icon" type="image/png" href="./img/monkey_narotu.png">
+    <link rel="stylesheet" type="text/css" href="./css/adminpage.css">
+    <script src="./Js/adminpage.js"></script>
+</head>
+<body>
+    
+
+
+    <div class="worldmap" >
+    </div>
+    <div class="noir">
+    </div>
+
+    <a href="./index.php">
+        <div class="buttonback">
+            <img src="./img/97591.svg" class="imgback" style="height: 80%; width: 80%;">
+        </div>
+    </a>
+
+    <div class="main" >
+        <div class="menu" >
+            <a href="" class="choix" >    
+                <div>
+                    <h2>Ajouter un constructeur</h2>
+                </div>
+            </a>
+            <a href="" class="choix" >    
+                <div>
+                    <h2>Ajouter une voiture</h2>
+                </div>
+            </a>
+            <a href="" class="choix" >    
+                <div>
+                    <h2>Suprimer un constructeur</h2>
+                </div>
+            </a>
+            <a href="" class="choix" >    
+                <div>
+                    <h2>Suprimer une voiture</h2>
+                </div>
+            </a>
+            <a href="" class="choix" >    
+                <div>
+                    <h2>Suprimer toute la BDD</h2>
+                </div>
+            </a>
+            <a href="" class="choix" >    
+                <div>
+                    <h2>Renitialiser la BDD</h2>
+                </div>
+            </a>
+
+
+
+
+
+        </div>
+        <div class="formulaire">
+
+
+        </div>
+</div>
+
+
+</body>
+</html>
