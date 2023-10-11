9cba9016cb0733a4346ac1f0cd2113679850a288
jules.corneille <jules.corneille@uha.fr>
2023-10-11 15:57:20 +0200

    ajout d'image sur les bouton supprimer et renitialiser

--- Js/adminpage.js
+++ Js/adminpage.js
@@ -1,16 +1,30 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.choix');
+
+    const choisiaffDivs = document.querySelectorAll('.choisiaff');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const targetId = button.getAttribute('data-target');
+
+            choisiaffDivs.forEach(div => {
+                div.style.display = 'none';
+            });
+
+            const targetDiv = document.getElementById(targetId);
+            if (targetDiv) {
+                targetDiv.style.display = 'block';
+            }
+        });
+    });
 
 
-    const resetButton = document.getElementById('res_bdd');
+    const resetButton = document.getElementById('resetbdd');
     const suprimerButton = document.getElementById('supr_all');
-
-
 
     resetButton.addEventListener('click', function () {
         const xhr = new XMLHttpRequest();
-        console.log("click")
+        console.log("click");
         xhr.open('POST', './bdd/bdd_res.php', true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
@@ -21,20 +35,16 @@ document.addEventListener('DOMContentLoaded', function () {
         xhr.send();
     });
 
-    
     suprimerButton.addEventListener('click', function () {
         const xhr = new XMLHttpRequest();
-        console.log("click")
+        console.log("click");
         xhr.open('POST', './bdd/bdd_supr.php', true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
-
-                console.log('Base de données supprimer avec succès.');
-                alert("La base de donnée a été supprimer avec succès.");
+                console.log('Base de données supprimée avec succès.');
+                alert("La base de donnée a été supprimée avec succès.");
             }
         };
         xhr.send();
     });
-
-
 });
--- adminpage.php
+++ adminpage.php
@@ -25,42 +25,69 @@
 
     <div class="main" >
         <div class="menu" >
-            <button class="choix" >    
+            <button class="choix" data-target="addc" >    
                 <div>
                     <h2>Ajouter un constructeur</h2>
                 </div>
             </button>
-            <button class="choix" >    
+            <button class="choix" data-target="addv" >    
                 <div>
                     <h2>Ajouter une voiture</h2>
                 </div>
             </button>
-            <button class="choix" >    
+            <button class="choix" data-target="supprc" >    
                 <div>
                     <h2>Suprimer un constructeur</h2>
                 </div>
             </button>
-            <button class="choix" >    
+            <button class="choix" data-target="supprv">    
                 <div>
                     <h2>Suprimer une voiture</h2>
                 </div>
             </button>
-            <button class="choix" id="supr_all" >    
+            <button class="choix" id="supr_all" data-target="supprimer_bdd" >    
                 <div>
                     <h2>Suprimer toute la BDD</h2>
                 </div>
             </button>
-            <button class="choix" id="res_bdd">    
+            <button class="choix" id="resetbdd" data-target="res_bdd">    
                 <div>
                     <h2>Renitialiser la BDD</h2>
                 </div>
             </button>
         </div>
-        <div class="formulaire">
+        <div class="choisi">
+            <div class="choisiaff" id="addc" >
 
+            </div>
+            <div class="choisiaff" id="addv" >
 
+            </div>
+            <div class="choisiaff" id=" supprc" >
 
+            </div>
+            <div class="choisiaff" id="supprv">
+
+            </div>
+            <div class="choisiaff" id="supprimer_bdd">
+                <div class="imgch"  >
+                    <img src="./img/img_admin/supprimer-la-base-de-donnees.png" class="logo_admin" style="filter: brightness(0) invert(1) grayscale(100%) sepia(0%) saturate(0%);" >
+                    <h2>
+                        La base de donnée a été supprimer
+                    </h2>
+                </div>
+            </div>
+            <div class="choisiaff" id="res_bdd">
+                <div class="imgch" >
+                    <img src="./img/img_admin/reset-base-de-donnees.png" class="logo_admin" style=" filter: brightness(0) invert(1) grayscale(100%) sepia(0%) saturate(0%);" >
+                    <h2>
+                        La base de donnée a été rénitialisé avec les données des API
+                    </h2>
+                </div>
+            </div>
         </div>
+            
+
 </div>
 </body>
 </html>
