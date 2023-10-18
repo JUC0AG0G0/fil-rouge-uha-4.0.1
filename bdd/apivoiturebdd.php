664874c50363a9929debe9335aa031bf6f4d63ae
jules.corneille <jules.corneille@uha.fr>
2023-10-18 11:07:55 +0200

    changement des noms des variables

--- bdd/apivoiturebdd.php
+++ bdd/apivoiturebdd.php
@@ -1,11 +1,11 @@
 <?php
 $bdd = new PDO('mysql:host=localhost;dbname=fil_rouge_401_Corneille_Jules', 'Fil_Rouge_Jules_Conrneille', '1234');
 
-$pdoStat = $bdd->prepare('SELECT * FROM ApiVoitures ORDER BY nom ASC');
+$api_voitures = $bdd->prepare('SELECT * FROM ApiVoitures ORDER BY nom ASC');
 
-$excuteIsOk = $pdoStat->execute();
+$excuteIsOk = $api_voitures->execute();
 
-$voitures = $pdoStat->fetchAll();
+$voitures = $api_voitures->fetchAll();
 
 header('Content-Type: application/json');
 
