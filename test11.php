61fd04b7fded08a7ea3d3b1e86af0beccacd34e1
jules.corneille <jules.corneille@uha.fr>
2023-10-19 15:03:34 +0200

    fonctionnement en dur du trie des pays

--- test11.php
+++ test11.php
@@ -17,49 +17,50 @@
     </a>
     <div class="scroller">
         <?php
-
-
-        $europearabeworld = $_GET['arabworld'];
-        $europecontinentaleurope = $_GET['continentaleurope'];
-        $europeasiaoceania = $_GET['asiaoceania'];
-        $europeasiaoceania = $_GET['europecentralasia'];
-        $europeasiaoceania = $_GET['latinamericacaribbean'];
-        $europeasiaoceania = $_GET['northamerica'];
-        $europeasiaoceania = $_GET['subsaharanafrica'];
-        
+       
       
         $bdd = new PDO('mysql:host=localhost;dbname=fil_rouge_401_Corneille_Jules', 'Fil_Rouge_Jules_Conrneille', '1234');
 
-        // Récupération de la liste des constructeurs par pays
-        $constructeurQuery = $bdd->prepare('SELECT * FROM ApiConstructeur ORDER BY pays, nom ASC');
+
+        $urleuropearabeworld = $_GET['arabworld'];
+        $urleuropecontinentaleurope = $_GET['continentaleurope'];
+        $urleuropeasiaoceania = $_GET['asiaoceania'];
+        $urleuropeasiaoceania = $_GET['europecentralasia'];
+        $urleuropeasiaoceania = $_GET['latinamericacaribbean'];
+        $urleuropeasiaoceania = $_GET['northamerica'];
+        $urleuropeasiaoceania = $_GET['subsaharanafrica'];
+
+        $constructeurQuery = $bdd->prepare('SELECT * FROM ApiConstructeur 
+        JOIN ApiContinent ON ApiConstructeur.pays = ApiContinent.nom_pays 
+        WHERE ApiContinent.arabworld = 1 OR ApiContinent.continentaleurope = 1 
+        ORDER BY ApiConstructeur.pays, ApiConstructeur.nom ASC');
         $constructeurQuery->execute();
         $constructeurs = $constructeurQuery->fetchAll();
-
-
+        
         $currentCountry = "";
-
+        
         foreach ($constructeurs as $constructeur) {
             if ($constructeur['pays'] != $currentCountry) {
                 if ($currentCountry != "") {
                     echo '</div><hr></div>';
                 }
                 $currentCountry = $constructeur['pays'];
-
+        
                 // Recherche du lien du drapeau du pays dans la table ApiContinent
                 $drapeauQuery = $bdd->prepare('SELECT drapeaupays FROM ApiContinent WHERE nom_pays = ?');
                 $drapeauQuery->execute([$currentCountry]);
                 $drapeau = $drapeauQuery->fetch();
-
+        
                 echo '<div class="partie">';
                 echo '<div class="pays"><img src="' . htmlspecialchars($drapeau['drapeaupays'], ENT_QUOTES, 'UTF-8') . '" class="imgpays"><h1 class="txtpays">' . $constructeur['pays'] . '</h1></div>';
                 echo '<div class="marques">';
             }
-
+        
             echo '<a href="./template_marque.php?id=' . $constructeur['id'] . '"><div class="boutondiv">';
             echo '<img src="./img/logo_marque/' . htmlspecialchars($constructeur['nom'], ENT_QUOTES, 'UTF-8') . '.svg" class="logoimg">';
             echo '</div></a>';
         }
-
+        
         if ($currentCountry != "") {
             echo '</div><hr></div>';
         }
