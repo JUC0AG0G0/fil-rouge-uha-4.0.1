4f852a046d8fb2af1e7358f3802abdffe8e81625
jules.corneille <jules.corneille@uha.fr>
2023-10-25 03:20:03 +0200

    ajout des fichier locaux

--- test12.php
+++ test12.php
@@ -34,20 +34,70 @@ if (isset($_POST["ajouterc"])) {
     $nom = $_POST['nom'];
     $creation = $_POST['creation'];
     $fondateur = $_POST['fondateur'];
-    $pays = $_POST['cars']; // Utilisez le nom correct du champ de sélection
+    $pays = $_POST['pays'];
 
-    $sql = "INSERT INTO `ApiConstructeur`(`nom`, `creation`, `fondateur`, `pays`) VALUES (:nom, :creation, :fondateur, :pays)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':nom', $nom);
-    $stmt->bindParam(':creation', $creation);
-    $stmt->bindParam(':fondateur', $fondateur);
-    $stmt->bindParam(':pays', $pays);
+    // Emplacement des fichiers de destination
+    $logoDestination = './img/logo_marque/';
+    $videoDestination = './video/marques/';
 
-    try {
-        $stmt->execute();
-        echo "Constructeur ajouté avec succès.";
-    } catch (PDOException $e) {
-        echo "Erreur lors de l'ajout du constructeur : " . $e->getMessage();
+    // Vérifie si le fichier "logo" a été soumis
+    if (isset($_FILES['logo']) && $_FILES['logo']['error'] === UPLOAD_ERR_OK) {
+        $logoFileName = $_FILES['logo']['name'];
+        $logoTempFile = $_FILES['logo']['tmp_name'];
+        $logoFilePath = $logoDestination . $logoFileName;
+    } else {
+        echo "Erreur : Le fichier du logo n'a pas été correctement soumis.";
+        exit;
+    }
+
+    // Vérifie si le fichier "video" a été soumis
+    if (isset($_FILES['video']) && $_FILES['video']['error'] === UPLOAD_ERR_OK) {
+        $videoFileName = $_FILES['video']['name'];
+        $videoTempFile = $_FILES['video']['tmp_name'];
+        $videoFilePath = $videoDestination . $videoFileName;
+    } else {
+        echo "Erreur : Le fichier de la vidéo n'a pas été correctement soumis.";
+        exit;
+    }
+
+    // Vérifiez si les répertoires de destination existent, sinon, créez-les
+    if (!file_exists($logoDestination)) {
+        mkdir($logoDestination, 0777, true);
+    }
+    if (!file_exists($videoDestination)) {
+        mkdir($videoDestination, 0777, true);
+    }
+
+    // Affichez des messages de débogage pour vérifier les valeurs
+    echo "Nom: " . $nom . "<br>";
+    echo "Année de création: " . $creation . "<br>";
+    echo "Fondateur: " . $fondateur . "<br>";
+    echo "Pays d'origine: " . $pays . "<br>";
+
+    // Affichez des informations sur les fichiers téléchargés
+    echo "Nom du fichier du logo: " . $logoFileName . "<br>";
+    echo "Nom du fichier de la vidéo: " . $videoFileName . "<br>";
+
+    // Déplacez les fichiers téléchargés vers les répertoires de destination
+    if (move_uploaded_file($logoTempFile, $logoFilePath) && move_uploaded_file($videoTempFile, $videoFilePath)) {
+        echo "Fichiers téléchargés avec succès.<br>";
+        // Les fichiers ont été téléchargés avec succès, ajoutez maintenant les données à la base de données
+        $sql = "INSERT INTO `ApiConstructeur`(`nom`, `creation`, `fondateur`, `pays`) VALUES (:nom, :creation, :fondateur, :pays)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':creation', $creation);
+        $stmt->bindParam(':fondateur', $fondateur);
+        $stmt->bindParam(':pays', $pays);
+
+
+        try {
+            $stmt->execute();
+            echo "Constructeur ajouté avec succès.";
+        } catch (PDOException $e) {
+            echo "Erreur lors de l'ajout du constructeur : " . $e->getMessage();
+        }
+    } else {
+        echo "Erreur lors du téléchargement des fichiers.";
     }
 }
 ?>
@@ -60,7 +110,7 @@ if (isset($_POST["ajouterc"])) {
     <title>test 12</title>
 </head>
 <body>
-    <form method="POST" action="">
+    <form method="POST" action="" enctype="multipart/form-data">
         <h2>Nom :</h2>
         <input type="text" name="nom" />
 
@@ -71,11 +121,17 @@ if (isset($_POST["ajouterc"])) {
         <input type="text" name="fondateur" />
 
         <h2>Pays d'origine :</h2>
-        <select name="cars"> <!-- Utilisez le nom correct du champ de sélection -->
+        <select name="pays">
             <?php foreach ($payscontinent as $pays) : ?>
                 <option value="<?php echo $pays['nom_pays']; ?>"><?php echo $pays['nom_pays']; ?></option>
             <?php endforeach; ?>
         </select>
+
+        <h2>Logo :</h2>
+        <input type="file" name="logo" accept=".svg" />
+
+        <h2>Video :</h2>
+        <input type="file" name="video" accept=".mp4" />
 
         <br><br>
 
