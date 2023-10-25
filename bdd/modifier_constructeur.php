48cefd84125b3eaca96efe69b54bbeb2f02f0256
jules.corneille <jules.corneille@uha.fr>
2023-10-25 14:50:18 +0200

    enregistrement des modifications aporté sur les constructeur OK

--- bdd/modifier_constructeur.php
+++ bdd/modifier_constructeur.php
@@ -19,7 +19,23 @@ if (isset($_POST["modifier_constructeur"])) {
     $fondateur = $_POST['fondateur'];
     $pays = $_POST['pays'];
 
-    // Effectuez la mise à jour du constructeur dans la base de données en utilisant $constructeurId, $creation, $fondateur, $pays, etc.
+    // Requête SQL pour mettre à jour le constructeur
+    $sql = "UPDATE ApiConstructeur SET creation = :creation, fondateur = :fondateur, pays = :pays WHERE id = :constructeurId";
+    
+    $stmt = $bdd->prepare($sql);
+    $stmt->bindParam(':creation', $creation);
+    $stmt->bindParam(':fondateur', $fondateur);
+    $stmt->bindParam(':pays', $pays);
+    $stmt->bindParam(':constructeurId', $constructeurId, PDO::PARAM_INT);
 
-    // Redirigez l'utilisateur vers une page appropriée après la modification.
+    try {
+        $stmt->execute();
+        // La mise à jour a réussi, redirigez l'utilisateur vers une page de confirmation ou ailleurs.
+    } catch (PDOException $e) {
+        // La mise à jour a échoué, gérez l'erreur, par exemple, renvoyez un message d'erreur.
+        echo "Erreur lors de la mise à jour du constructeur : " . $e->getMessage();
+    }
+    header('Location: ../adminpage.php'); // Redirigez vers la même page
+    exit; // Assurez-vous de quitter le script pour éviter l'exécution continue
+
 }
