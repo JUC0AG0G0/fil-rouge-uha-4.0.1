a190617e675709b388e8f71547e30a71d9ed48f0
jules.corneille <jules.corneille@uha.fr>
2023-10-26 15:02:32 +0200

    systeme de pagination fonctionnel seulement en europe

--- index.php
+++ index.php
@@ -12,17 +12,17 @@
         <div class="selection">
             <div class="continent">
                 <div class="mmm">
-                    <a href="./america.php" class="bc"><h1>AMERICA</h1></a>
+                    <a href="./america.php?page=1" class="bc"><h1>AMERICA</h1></a>
                 </div>
             </div>
             <div class="continent">
                 <div class="mmm">
-                    <a href="./europe.php" class="bc"><h1>EUROPE</h1></a>
+                    <a href="./europe.php?page=1" class="bc"><h1>EUROPE</h1></a>
                 </div>
             </div>
             <div class="continent">
                 <div class="mmm">
-                    <a href="./asia.php" class="bc"><h1>ASIA-PACIFIC</h1></a>
+                    <a href="./asia.php?page=1" class="bc"><h1>ASIA-PACIFIC</h1></a>
                 </div>
             </div>
         </div>
