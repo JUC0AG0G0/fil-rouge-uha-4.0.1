50d84c2ca1c789cd61e627d3b3ea909b40b8dc7f
jules.corneille <jules.corneille@uha.fr>
2023-10-26 15:23:57 +0200

    modification du titre des pages

--- america.php
+++ america.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste des Constructeurs et Voitures</title>
+    <title>Amerique</title>
     <link rel="icon" type="image/png" href="./img/monkey_narotu.png">
     <link rel="stylesheet" type="text/css" href="./css/continent.css">
 </head>
